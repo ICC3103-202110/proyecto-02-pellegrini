e1999e57e10f208d39a1b18f0399dffee83ff509
camilaphenderson <cvpellegrini@miuandes.cl>
2021-06-15 16:04:25 -0400

    avances

---
+++ model.js
@@ -0,0 +1,10 @@
+initModel = {
+    city : 'Santiago' ,
+    temp : 0 ,
+    max : 0 ,
+    min : 0
+}
+
+module.exports = {
+    initModel
+}
--- view.js
+++ view.js
@@ -2,7 +2,7 @@ const figlet = require('figlet')
 const chalk = require('chalk')
 const inquirer = require('inquirer')
 
-function getTitle(){
+function createTitle(){
     return chalk.yellow(
         figlet.textSync(
             'Weather app',
@@ -14,4 +14,14 @@ function getTitle(){
     )
 }
 
-console.log(getTitle())
+function createTable(model){
+    const {city} = model
+    const {temp} = model
+    const {max} = model
+    const {min} = model
+    return [
+        { 'City': city, 'Temp': temp, 
+        'Max': max, 'Min':min}
+    ]
+
+}
