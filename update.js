5ac04bc05a7e150a3094632d89895f0992b529f8
camilaphenderson <cvpellegrini@miuandes.cl>
2021-06-16 15:20:48 -0400

    avances

--- update.js
+++ update.js
@@ -1,36 +1,36 @@
 const { mainInput, addCity, updateCity, deleteCity } = require("./view");
 
 function addCityUpdate(model){
-    input1 = addCity.addCity
+    input1 = await addCity(model)
     const updatedModel= model.cities;
     updatedModel.push({city: input1, temp: temp1, max: max1, min: min1})
     updatedModel.names.push(input1)
     return updatedModel;
 }
 function updateCityUpdate(model){
-    input2 = updateCity.updateCity
+    input2 = await updateCity(model)
     const updatedModel= model.cities;
     updatedModel.push({city: input1, temp: temp1, max: max1, min: min1})
     return 
 }
 
 function deleteCityUpdate(model){
-    input3 = deleteCity.deleteCity
-    const updatedModel= model;
-    const index=model.city.indexOf(input3);
+    input3 = await deleteCity(model)
+    const updatedModel= model.cities;
+    const index= model.names.indexOf(input3);
     temporalModel.cities.splice(index,1);
     temporalModel.names.splice(index,1);
     return temporalModel;
 }
 
 function update (input,model){
-    if (mainInput.Main === 'Add City'){
+    if (mainInput.choices === 'Add City'){
         return addCityUpdate
     }
-    if (mainInput.Main === 'Update City'){
+    if (mainInput.choices === 'Update City'){
         return updateCityUpdate //DEFINIR ESTA FUNCION
     }
-    if (mainInput.Main === 'Delete City'){
+    if (mainInput.choices === 'Delete City'){
         return deleteCityUpdate // DEFINIR ESTA FUNCION 
     }    
 
