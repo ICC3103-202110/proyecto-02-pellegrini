3ca6297a4c02280f077f7f9dc47ad04636e385a1
camilaphenderson <cvpellegrini@miuandes.cl>
2021-06-18 15:04:45 -0400

    intento conectando api, falta arreglar app.js

--- model.js
+++ model.js
@@ -1,5 +1,5 @@
 initModel = {
-    cities : [],
+    info : [],
     names : []
 }
 
--- update.js
+++ update.js
@@ -1,6 +1,7 @@
 const { mainInput, addCity, updateCity, deleteCity } = require("./view");
+const {axios} = require('axios')
 
-function addingCity(names,city){
+function addingName(names,city){
     newNames = names.push(city)
     return newNames
 }
@@ -14,33 +15,80 @@ function addInfo(cities,row){
     addedInfo = a.push(row)
     return addedInfo 
 }
+function connectApi(selected,Key)
+    link = axios.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+Key+'&units=metric')
+    return link 
 
-function update (action,selection,model){
+
+function update (chosen,selected,info,names){
     // sumando una ciudad con numeros aleatorios 
-    if (action === 'Add City'){
-        const updatedModel= model;
-        var max1= Math.floor(Math.random()*30)+10
-        var min1= Math.floor(Math.random()*10)
-        var temp1 = (max1 + min1)/2 
-        updatedModel.push({city: selection, temp: temp1, max: max1, min: min1})
+    if (chosen === 'Add City'){
+        const updatedModel= info ;
+        newNamesList = addingName(names,selected)
+        conInfo = connectApi(selected,'6fa5c94be7a25d05a112fea1ae1bae13')
+        x = await conInfo.then((response) => {
+            temp1 = response.data.main.temp
+            max1 = response.data.main.temp_max
+            min1 = response.data.main.temp_min
+            error = ""
+            })
+            .catch(function () {
+              error = 'error in finding city'
+              console.log(error);
+            })
+            if (error === 'error in finding city'){
+                max1r = Math.floor(Math.random()*30)+10
+                min1r = Math.floor(Math.random()*max1r)-10
+                temp1r = (max1r + min1r)/2 
+                row = row(selected,temp1r,max1r,min1r)
+                updatedModel = addRow(row)  
+            }
+            else if (error != 'error in finding city'){
+                row = row(selected,temp1,max1,min1)
+                updatedModel = addRow(row)  
+            }
+
+
         return updatedModel
     }
-    else if (action === 'Update City'){
-        const updatedModel= model;
-        var max1= Math.floor(Math.random()*30)+10
-        var min1= Math.floor(Math.random()*10)
-        var temp1 = (max1 + min1)/2 
-        updatedModel.push({city: selection, temp: temp1, max: max1, min: min1})
+    else if (chosen === 'Update City'){
+        const updatedModel= info;
+        const index= names.indexOf(selected);
+        x = await conInfo.then((response) => {
+            temp1 = response.data.main.temp
+            max1 = response.data.main.temp_max
+            min1 = response.data.main.temp_min
+            error = ""
+            })
+            .catch(function () {
+              error = 'error in finding city'
+              console.log(error);
+            })
+            if (error === 'error in finding city'){
+                max1r = Math.floor(Math.random()*30)+10
+                min1r = Math.floor(Math.random()*max1r)-10
+                temp1r = (max1r + min1r)/2 
+                row = row(selected,temp1r,max1r,min1r)
+                updatedModel[index] = row  
+            }
+            else if (error != 'error in finding city'){
+                row = row(selected,temp1,max1,min1)
+                updatedModel[index] = row 
+            }
+
+
+
         return updatedModel
     }
     else if (action === 'Delete City'){
-        const updatedModel= model;
-        const index= model.city.indexOf(selection);
+        const updatedModel= info; 
+        const updatedNames = names; 
+        const index= names.indexOf(selected);
         updatedModel.splice(index,1);
-        return updatedModel;
-    }    
-}
-    
+        updatedNames.splice(index,1)
+        return updatedModel, updatedNames
+}    
+   
 module.exports = {
     update
 }
